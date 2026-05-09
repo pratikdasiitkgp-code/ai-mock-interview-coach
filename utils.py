09f0bb0c32367b0e28f8dcc2a641bdb49533e814
Pratik Das <pratikdasiitkgp@gmail.com>
2026-05-09 15:14:41 +0530

    fixed requirements

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
-import os
 import streamlit as st
 
 from openai import OpenAI
 
-client = OpenAI(api_key=st.secrets("OPENAI_API_KEY"))
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def call_llm(messages, temperature=0.7):
     response = client.chat.completions.create(
