39bd1632f98799d1da7f5067845d4e96b427fd7a
Pratik Das <pratikdasiitkgp@gmail.com>
2026-05-09 14:47:31 +0530

    fixed streamlit secrets

--- utils.py
+++ utils.py
@@ -1,10 +1,9 @@
 import os
-from dotenv import load_dotenv
+import streamlit as st
+
 from openai import OpenAI
 
-load_dotenv()
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=st.secrets("OPENAI_API_KEY"))
 
 def call_llm(messages, temperature=0.7):
     response = client.chat.completions.create(
